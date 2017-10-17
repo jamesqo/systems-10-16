b783d0369579a7e730ea37165862d5fe7212a2b0
James Ko <jamesqko@gmail.com>
2017-10-17 10:36:05 -0400

    Define music_lib.h interface

--- music_lib.h
+++ music_lib.h
@@ -7,3 +7,13 @@ song_node* music_lib_search_for_song(char* name);
 song_node* music_lib_search_for_artist(char* artist);
 
 void music_lib_print_entries_with_first_letter(char letter);
+
+void music_lib_print_songs_of_artist(char* artist);
+
+void music_lib_print_whole_library();
+
+void music_lib_shuffle();
+
+void music_lib_delete_song(char* name);
+
+void music_lib_delete_whole_library();
