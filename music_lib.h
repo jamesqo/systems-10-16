ee3c216b32b6a5e7f74005d6e2e26bcf6fb64c4e
James Ko <jamesqko@gmail.com>
2017-10-17 10:30:39 -0400

    Add several new files

---
+++ music_lib.h
@@ -0,0 +1,9 @@
+#include "linked_list.h"
+
+void music_lib_add_song(char* name, char* artist);
+
+song_node* music_lib_search_for_song(char* name);
+
+song_node* music_lib_search_for_artist(char* artist);
+
+void music_lib_print_entries_with_first_letter(char letter);
